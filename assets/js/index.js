61d1963dbe98819aad3a3d72c1f482b094dd0949
IrynaKr1 <krutsiryna@gmail.com>
2025-08-29 19:22:12 +0200

    Add js veryfication

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+const INPUTS_REG_EXP_VALID = {
+  'first-name': /^[A-Z][a-z]{2,19}(-[A-Z][a-z]{2,19})?$/,
+  'last-name': /^[A-Z][a-z]{2,19}(-[A-Z][a-z]{2,19})?$/,
+  email: /^.*@.+$/,
+  'phone-code': /\+[1-9]\d{0,3}/,
+  'phone-number': /\d{2,4}/,
+  'phone-end': /\d{4,7}/,
+  'message-subject': /^.+$/,
+  message: /^.{10,500}$/,
+};
+
+const inputs = document.querySelectorAll('input, select, textarea');
+
+inputs.forEach((i) => i.addEventListener('input', inputHandler));
+
+function inputHandler(e) {
+  const fieldName = e.target.name;
+  const fieldValue = e.target.value;
+  if (INPUTS_REG_EXP_VALID[fieldName]) {
+    if (INPUTS_REG_EXP_VALID[fieldName].test(fieldValue)) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+    } else {
+      e.target.classList.remove('valid');
+      e.target.classList.add('invalid');
+    }
+  }
+}
